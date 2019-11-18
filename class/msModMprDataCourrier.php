980f9e948057223937d146c8fabd264a091c7ed2
Bertrand Boutillier <b.boutillier@gmail.com>
2019-11-18 11:26:18 +0100

    fix: calc IMC

--- class/msModMprDataCourrier.php
+++ class/msModMprDataCourrier.php
@@ -54,7 +54,7 @@ class msModMprDataCourrier
       }
       // résoudre le problème de l'IMC
       unset($d['imc']);
-      if(isset($d['poids'],$d['taillePatient'])) $d['imc']=msModMedgeCalcMed::imc($d['poids'],$d['taillePatient']);
+      if(isset($d['poids'],$d['taillePatient'])) $d['imc']=msModBaseCalcMed::imc($d['poids'],$d['taillePatient']);
 
     }
 
